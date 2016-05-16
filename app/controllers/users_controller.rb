8de38000dc987af7afc019ad09313a4fe323d6bc
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2016-05-16 10:11:27 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -20,10 +27,50 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    #@user = User.find(params[:id])
+  end
+
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
+  #Confirms a logged in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "You must be logged in to view this page."
+      redirect_to login_url
+    end
+  end
+
+  #Confirms correct user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
   private
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+    #Confirms user is admin, redirects to root if not
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
 end
