a8ea45fc509cc665da48763e1343e98362644cb2
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2016-06-27 14:46:34 -0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
            has_many :microposts, dependent: :destroy
+           has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+           has_many :following, through: :active_relationships, source: :followed
+           has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+           has_many :followers, through: :passive_relationships
+
 
 	#Returns the hash digest of the given string
 	def User.digest(string)
@@ -40,9 +45,25 @@ class User < ActiveRecord::Base
     		update_attribute(:remember_digest, nil)
     	end
 
-           #Defines a proto-feed
+           #Returns a user's status feed
            def feed
-            Micropost.where("user_id = ?", id)
+            following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+            Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", following_ids: following_ids, user_id: id)
            end
+
+           #Follows a user
+            def follow(other_user)
+                active_relationships.create(followed_id: other_user.id)
+            end
+
+            #Unfollows a user
+            def unfollow(other_user)
+                active_relationships.find_by(followed_id: other_user.id).destroy
+            end
+
+            #Returns true if user is following another user
+            def following?(other_user)
+                following.include?(other_user)
+            end
 
 end
